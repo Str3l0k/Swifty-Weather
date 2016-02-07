6698a72d3bf70909ed26c7a7403dec22e1d29caf
Stefan Gerasch <StefanGerasch1991@gmail.com>
2016-02-07 19:21:52 +0100

    Added Size Class for iPad (Regular x Regular)

--- Weather/ForeCastViewController.swift
+++ Weather/ForeCastViewController.swift
@@ -14,6 +14,8 @@ class ForeCastViewController : UITableViewController{
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.estimatedRowHeight = 44
+        tableView.rowHeight = UITableViewAutomaticDimension
         // Do any additional setup after loading the view, typically from a nib.
     }
     
